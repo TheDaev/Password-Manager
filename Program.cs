7c0ed4e3fbed11fea0819781ce61575e0b5b8baf
realdenis <realdenismj@gmail.com>
2023-03-18 19:41:26 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace passwordmanager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] == "setpass")
+            {
+                File.WriteAllText($"passfor{args[1]}", args[2]);
+                Console.WriteLine($"Password for {args[1]} has been created successfully!");
+                Console.ReadLine();
+            }
+            else if (args[0] == "seepass")
+            {
+                
+                if (File.Exists($"passfor{args[1]}"))
+                {
+                    string idk = File.ReadAllText($"passfor{args[1]}");
+                    Console.WriteLine($"The password for {args[1]} is {idk}");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine($"Password for {args[1]} not found");
+                }
+                
+                
+            }
+        }
+    }
+}
